8c778f15c50193c878d3deb51e418154329a0daa
rdang2 <robin.dang@booking.com>
2023-11-17 18:55:32 +0100

    add sample data

--- lib/data.ts
+++ lib/data.ts
@@ -6,6 +6,10 @@ import { CgWorkAlt } from 'react-icons/cg';
 import { FaReact } from 'react-icons/fa';
 import { LuGraduationCap } from 'react-icons/lu';
 
+/**
+ * SAMPLE DATA SET
+ */
+
 export const links = [
   {
     name: 'Home',
