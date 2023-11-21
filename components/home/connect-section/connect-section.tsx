73d5b5e3f9056bf49de0fde59c31873bd96d6235
rdang2 <robin.dang@booking.com>
2023-11-21 14:03:53 +0100

    add npmrc, new CV

--- components/home/connect-section/connect-section.tsx
+++ components/home/connect-section/connect-section.tsx
@@ -29,7 +29,7 @@ export default function ConnectSection() {
       </Link>
 
       <a
-        href="/CV.pdf"
+        href="/Robin_Dang_CV.pdf"
         download
         className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
       >
