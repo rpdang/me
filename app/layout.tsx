e825beda756641625fce69ed9e9b525ebd73da67
KieuMy <macair@kieus-mbp.home>
2023-11-21 19:40:48 +0100

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import ActiveSectionContextProvider from '@/context/active-section-context';
 import ThemeContextProvider from '@/context/theme-context';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
+import {Analytics} from "@vercel/analytics/react"
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -35,6 +36,7 @@ export default function RootLayout({
             <Toaster position="top-right" />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
+        <Analytics/>
       </body>
     </html>
   );
