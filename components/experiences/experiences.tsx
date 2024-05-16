2d0a4613c85e4600cc19d2d70280d1e0490557e1
rdang2 <robin.dang@booking.com>
2024-05-16 23:54:50 +0200

    adjust experience in view header threshold

--- components/experiences/experiences.tsx
+++ components/experiences/experiences.tsx
@@ -8,7 +8,7 @@ import SectionHeading from '../section-heading/section-heading';
 import Experience from './experience-element';
 
 export default function Experiences() {
-  const { ref } = useSectionInView('Experience');
+  const { ref } = useSectionInView('Experience', 0.2);
 
   return (
     <section ref={ref} id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
