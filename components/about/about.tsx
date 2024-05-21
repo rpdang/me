64cc111b92e11a9e9c8d963bea3249f439040b86
rdang2 <robin.dang@booking.com>
2024-05-21 12:44:22 +0200

    update about me

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -18,34 +18,21 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        Seasoned software engineer with an expansive skill set in{' '}
-        <span className="font-medium">full-stack development</span>. I am driven
-        by personal commitment to continual growth and learning, with a passion
-        for software architecture. I am actively pursuing the latest in
-        technology, my expertise spans everything between databases to browsers.
-        My core lies in{' '}
-        <span className="font-medium underline">
-          React, Next.js, Java, Spring Boot, Python, FastAPI
-        </span>{' '}
-        I eagerly embrace new experiences and challenges, fueled by enthusiasm
-        to explore all the opportunities life has to offer. Basically, I'm just
-        excited to live life.
+        I'm a software engineer with an expansive skill set in{' '}
+        <span className="font-medium">full stack development</span>. I'm
+        passionate about innovation, designing and building highly scalable
+        solutions from the ground up. With a solid foundation in both frontend
+        and backend development, my expertise spans from everything between
+        database management to user interface creation, ensuring seamless,
+        efficient, and robust software systems.
       </p>
       <p>
         <span className="italic">Beyond coding,</span> you'll often find me
-        playing vibrant chords of my electric guitar, sweating it out at the
-        gym, experimenting with new recipes in the kitchen, and delving into
-        diverse cuisines. This zest for exploration is ingrained in my
-        bicultural background as a{' '}
-        <span className="font-medium">Sweden-born Vietnamese</span>{' '}
-        second-generation immigrant, where curiosity and adaptability have
-        become my greatest assets. I thrive on embracing diversity, continuously
-        learning, and growing alongside others. Notably, I've been fortunate
-        enough to be a proficient professional{' '}
-        <span className="font-medium"> handball</span> player, representing the
-        junior national team, and a professional{' '}
-        <span className="font-medium">DJ</span> before I was able to turn coffee
-        into elegant lines of code.
+        playing the electric guitar, sweating it out at the gym or experimenting
+        with new recipes in the kitchen. In a previous life, I was playing
+        handball on a professional level, representing the Swedish junior
+        national team, and I also worked as a professional{' '}
+        <span className="font-medium">DJ</span>.
       </p>
       <p className="italic mt-5 text-sm"> Robin Dang</p>
       <span className="italic text-sm">M.Sc. Computer Science</span>
